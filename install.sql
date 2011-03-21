2fc10c82f0a8efe1c140a0d6e3d2c44b655f1586
konfushus <kenny.roytman@gmail.com>
2011-03-20 19:36:43 -0500

    initial version.
    this is the installation file that configures this asynchronous library on the target system.

---
+++ install.sql
@@ -0,0 +1,24 @@
+
+whenever sqlerror exit -1 rollback;
+
+grant execute on dbms_crypto to &&install_user.
+/
+grant execute on dbms_lock to &&install_user.
+/
+grant execute on dbms_alert to &&install_user.
+/
+grant execute on dbms_scheduler to &&install_user.
+/
+
+grant create job to &&install_user.
+/
+grant create table to &&install_user.
+/
+grant create procedure to &&install_user.
+/
+grant select any dictionary to &&install_user.
+/
+
+
+grant alter system to &&dba_user.
+/
